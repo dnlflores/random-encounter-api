d25b924543b330aba94e073f417960d456fef697
Daniel Flores <daniel.flores4930@gmail.com>
2022-03-21 14:06:41 -0700

    updated package.json and package-lock.json

--- store/index.js
+++ store/index.js
@@ -1,1 +1,16 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+const rootReducer = combineReducers({
+    // reducers go here
+});
+
+const logger = require('redux-logger').default;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+
+const configureStore = (preloadedState) => {
+    return createStore(rootReducer, preloadedState, enhancer);
+};
+
+export default configureStore;
